19d9e6d028fbf97034dc4df8f8022a683ce3aeca
bangood <2419281635@qq.com>
2019-10-29 16:39:26 +0800

    no message

--- public/javascripts/wxshare.js
+++ public/javascripts/wxshare.js
@@ -32,8 +32,8 @@
             var shareInfo = {
                 title: '川航美食研究所',
                 desc: '川航美食研究所，不一样的飞机餐，快来测下你的美食偏好吧',
-                link: 'http://m.mgapp.com.cn/',
-                imgUrl: 'http://rs2.mgapp.com.cn/%E9%93%BE%E6%8E%A5%E5%9B%BE.png',
+                link: 'http://hi.sichuanair.com/',
+                imgUrl: 'http://hi.sichuanair.com/link.png',
             };
             wx.ready(function () {
                 //分享到朋友圈
